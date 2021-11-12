ab319416710b95c22da53b36bf06074eeaa5c0c4
jakobhaggstrom <64440975+jakobhaggstrom@users.noreply.github.com>
2021-11-12 09:46:54 +0100

    m

--- Lab1/Q15.m
+++ Lab1/Q15.m
@@ -1,9 +1,9 @@
 I = imread('craw.jpg');
 J = rgb2gray(I);
 K = imresize(J,[128 128]);
-L = uint8(zeros(132));
-L(3:130,3:130) = L(3:130,3:130) + K;
-imtool(L)
+L = uint8(mean2(J) * ones(132));
+L(3:130,3:130) = K;
+% imtool(L)
 Inew = zeros(128);
 for i = 3:130
     for j = 3:130
@@ -11,5 +11,14 @@ for i = 3:130
         Inew(i-2,j-2) = round(mean(B,'all'));
     end
 end
+
 Inew = single(Inew);
-imtool(Inew)
+imtool(Inew/255)
+
+I_final = single((Inew - single(K)) / 255);
+
+I6 = single(-(Inew - single(K)) / 255);
+
+imtool(I_final)
+
+
